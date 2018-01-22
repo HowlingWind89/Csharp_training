5c913ab8863a379a041bcc153a50151b7f42087d
HowlingWind89 <nameless89@mail.ru>
2018-01-22 22:04:14 +0200

    Тест для добавления контакта создан.

---
+++ Addressbook-web-tests/Addressbook-web-tests/ContactData.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAddressbookTests
+{
+    class ContactData
+    {
+        private string first;
+        private string last;
+
+        public ContactData(string first)
+        {
+            this.first = first;
+        }
+
+        public string FirstName
+        {
+            get
+            {
+                return first;
+            }
+
+            set
+            {
+                first = value;
+            }
+        }
+
+        public string LastName
+        {
+            get
+            {
+                return last;
+            }
+
+            set
+            {
+                last = value;
+            }
+        }
+    }
+   
+}
