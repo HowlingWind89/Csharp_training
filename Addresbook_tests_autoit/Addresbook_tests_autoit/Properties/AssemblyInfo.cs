2c80963121803b4da4b63497dbe0420527e1467d
HowlingWind89 <nameless89@mail.ru>
2018-03-10 19:55:33 +0200

    Лекция 8.1
    Лекция 8.1

---
+++ Addresbook_tests_autoit/Addresbook_tests_autoit/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Addresbook_tests_autoit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Addresbook_tests_autoit")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9cabb6c5-ff05-445f-ba05-476969eb241b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Addresbook_tests_autoit/Addresbook_tests_autoit/Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+
+namespace Addressbook_tests_autoit
+{
+    [TestFixture]
+    public class UnitTest1 : TestBase
+    {
+        [Test]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
