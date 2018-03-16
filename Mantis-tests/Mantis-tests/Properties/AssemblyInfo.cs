2d86724a921e0c15f714118c02d41be93bd92721
HowlingWind89 <nameless89@mail.ru>
2018-03-16 22:18:02 +0200

    Лекция 9.1
    Лекция 9.1

---
+++ Mantis-tests/Mantis-tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Mantis-tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Mantis-tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d8c69387-2a8b-4502-99c0-44ec20f43e17")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Mantis-tests/Mantis-tests/tests/AccoutCreationTests.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+using System.IO;
+
+namespace Mantis_tests
+{
+    [TestFixture]
+    public class AccoutCreationTests : TestBase
+    {
+        [OneTimeSetUp]
+        public void setupConfig()
+        {
+            app.Ftp.BackupFile("/config_inc.php");
+            using (Stream localFile = File.Open("config_inc.php", FileMode.Open))
+            {
+                app.Ftp.Upload("/config_inc.php", localFile);
+            }
+        }
+        [Test]
+        public void TestAccountRegistration()
+        {
+            AccountData account = new AccountData
+            {
+                Name = "Tester",
+                Password = "password",
+                Email = "testuser@localhost.localdomain"
+            };
+
+            app.Registration.Register(account);
+        }
+
+        [OneTimeTearDown]
+        public void restoreCofig()
+        {
+            app.Ftp.RestoreBackupFile("/config_inc.php");
+        }
+    }
+}
